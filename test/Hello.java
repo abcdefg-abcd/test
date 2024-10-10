fceeabddf86b6ad1bc45868e2b7d84088ecc3cdd
zmb <3354715485@qq.com>
2024-10-10 20:28:34 +0800

    张三创建项目并提交到远程Git仓库

---
+++ test/Hello.java
@@ -0,0 +1,13 @@
+ /**
+* ClassName: Hello
+* Package: PACKAGE_NAME
+* Description: 
+* @Author zzz
+* @Create 2024/10/10 17:47 
+* @Version 1.0   
+*/public class Hello {
+    public static void main(String args[]){
+
+        System .out.println("Hello world");
+    }
+}
