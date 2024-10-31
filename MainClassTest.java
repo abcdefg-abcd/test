3d186e0f5a74c2b6d807ee6e920a8b7fc08e055b
Aella <3346282542@qq.com>
2024-10-31 16:31:59 +0800

    张三第一次提交

---
+++ MainClassTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MainClassTest {
+    @Test
+    public void testApp() {
+        assertTrue("Testing MainClass", true);
+    }
+}
+
